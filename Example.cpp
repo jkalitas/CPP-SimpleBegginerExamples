394494e1639a051638d1d0d0633e56d71cd51212
José Silva <jsilva@streambolico.com>
2018-01-11 11:44:01 +0000

    Added Example super as super class.

---
+++ Example.cpp
@@ -0,0 +1,21 @@
+/**
+ * @header Example.h
+ *
+ * @brief Example is a class to be extendend where the example and it's
+ * information is properly explained while running.
+ *
+ * @author jkalitas 2017
+ *
+ * */
+#include "Example.h"
+
+/**
+ * A default constructor.
+ */
+Example::Example() {}
+
+/**
+ * A default destructor.
+ */
+Example::~Example() {}
+
---
+++ Example.h
@@ -0,0 +1,45 @@
+/**
+ * @header Example.h
+ *
+ * @brief Example is a class to be extendend where the example and it's
+ * information is properly explained while running.
+ *
+ * @author jkalitas 2017
+ *
+ * */
+
+#ifndef CPP_SIMPLEBEGINNEREXAMPLES_EXAMPLE_H
+
+#include <iostream>
+
+#define CPP_SIMPLEBEGINNEREXAMPLES_EXAMPLE_H
+
+class Example{
+public:
+    /**
+     * A default constructor.
+     */
+    Example();
+    /**
+     * A default destructor.
+     */
+    virtual ~Example();
+    /**
+     * Getter for example's name.
+     * @return the example's name.
+     */
+    virtual std::string getName(){};
+
+private:
+    /**
+     * Example's name.
+     */
+    std::string _name;
+    /**
+     * Run's the example.
+     */
+    virtual void runExample(){};
+};
+
+
+#endif //CPP_SIMPLEBEGINNEREXAMPLES_EXAMPLE_H
